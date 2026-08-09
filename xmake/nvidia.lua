833eee4abd32a24de45d573fed38684d4b51b724
Zhu Wei <117533999+trmbh@users.noreply.github.com>
2026-08-10 01:25:30 +0800

    Configure PIC CUDA device linking

--- xmake/nvidia.lua
+++ xmake/nvidia.lua
@@ -2,6 +2,11 @@ target("llaisys-device-nvidia")
     set_kind("static")
     set_languages("cxx17")
     add_rules("cuda")
+    set_policy("build.cuda.devlink", true)
+    if not is_plat("windows") then
+        add_cuflags("-Xcompiler=-fPIC")
+        add_culdflags("-Xcompiler=-fPIC")
+    end
     add_files("../src/device/nvidia/*.cu")
     add_cugencodes("native")
     on_install(function (target) end)
