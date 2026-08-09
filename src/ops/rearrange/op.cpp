91d46bb75dec43ab322ec1b9a33fd557955747dd
Zhu Wei <117533999+trmbh@users.noreply.github.com>
2026-08-09 22:57:03 +0800

    Implement CPU rearrange

--- src/ops/rearrange/op.cpp
+++ src/ops/rearrange/op.cpp
@@ -1,7 +1,21 @@
 #include "op.hpp"
 
+#include "../cpu_utils.hpp"
+
+#include <cstring>
+
 namespace llaisys::ops {
 void rearrange(tensor_t out, tensor_t in) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, in);
+    CHECK_SAME_DTYPE(out->dtype(), in->dtype());
+    CHECK_ARGUMENT(out->shape() == in->shape(), "rearrange shape mismatch");
+    ASSERT(out->isContiguous(), "rearrange output must be contiguous");
+    if (out->deviceType() != LLAISYS_DEVICE_CPU) EXCEPTION_UNSUPPORTED_DEVICE;
+    if (in->isContiguous()) {
+        std::memcpy(out->data(), in->data(), in->numel() * in->elementSize());
+    } else {
+        auto tmp = in->contiguous();
+        std::memcpy(out->data(), tmp->data(), in->numel() * in->elementSize());
+    }
 }
 } // namespace llaisys::ops
