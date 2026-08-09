31dd5ecebbc077274c86e9e35ef144a4d747f2c6
Zhu Wei <117533999+trmbh@users.noreply.github.com>
2026-08-10 02:41:50 +0800

    Implement native CUDA kernels: xmake/nvidia.lua

--- xmake/nvidia.lua
+++ xmake/nvidia.lua
@@ -11,3 +11,19 @@ target("llaisys-device-nvidia")
     add_cugencodes("native")
     on_install(function (target) end)
 target_end()
+
+target("llaisys-ops-nvidia")
+    set_kind("static")
+    add_deps("llaisys-tensor")
+    set_languages("cxx17")
+    add_rules("cuda")
+    set_policy("build.cuda.devlink", true)
+    if not is_plat("windows") then
+        add_cuflags("-Xcompiler=-fPIC")
+        add_culdflags("-Xcompiler=-fPIC")
+    end
+    add_files("../src/ops/nvidia/*.cu")
+    add_cugencodes("native")
+    on_install(function (target) end)
+target_end()
+
